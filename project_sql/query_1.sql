5a08406afa989cb1f29f69dbe5bb3446149bd9a6
OWUSU PRINCE BAAH <princeowusu4881@gmail.com>
2026-05-25 02:10:27 +0000

    new changes

--- project_sql/query_1.sql
+++ project_sql/query_1.sql
@@ -1,1 +1,17 @@
 --just an experiment to see if I can write a query that will return the number of orders for each customer, along with the customer's name and email address.
+SELECT *
+FROM april_job_postings;
+SELECT *
+FROM job_postings_fact;
+
+SELECT 
+    COUNT(job_id) AS number_of_available_jobs,
+    job_title_short,
+    job_location,
+    CASE 
+        WHEN job_location = 'Anywhere' THEN 'Remote'
+        WHEN job_location = 'New York' THEN 'Local' 
+        ELSE 'Onsite' 
+
+    END AS location_category
+    
