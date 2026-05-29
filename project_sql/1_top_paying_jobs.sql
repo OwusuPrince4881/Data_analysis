bba646a30ddd468d0d065c1a5e17df97d5bd60e2
OWUSU PRINCE BAAH <princeowusu4881@gmail.com>
2026-05-29 23:44:23 +0000

    more study update

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,7 @@
+--just an experiment to see if I can write a query that will return the number of orders for each customer, along with the customer's name and email address.
+SELECT *
+FROM april_job_postings;
+SELECT *
+FROM job_postings_fact;
+
+SELECT * FROM april_job_postings
